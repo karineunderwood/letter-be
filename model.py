1c0acc1f949fb3c03be9379e5b3edcfe3fb19da3
karineunderwood <karineunderwood23@gmail.com>
2022-04-18 14:57:39 -0700

    model.py reviewed on 4-18

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 """Models for letter to your future self app."""
 
-from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -9,14 +8,19 @@ class User(db.Model):
     """A user."""
 
     __tablename__ = "users"
+
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     fname = db.Column(db.String)
     lname = db.Column(db.String)
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String, unique=True)
 
-
+    letters = db.relationship("Letter", back_populates="user")
+    favorites = db.relationship("Letter", secondary="favorites", backref="user")
+    
     def __repr__(self):
+        """Show info about user."""
+
         return f'<User user_id={self.user_id} email={self.email}>'
 
 class Letter(db.Model):
@@ -29,15 +33,17 @@ class Letter(db.Model):
     letter_body = db.Column(db.String)
     creation_date = db.Column(db.DateTime)
     delivery_date = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     likes = db.Column(db.Integer)
     read = db.Column(db.Boolean)
     publish = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
 
     # create a relationship
     user = db.relationship("User", back_populates="letters")
-
+    
     def __repr__(self):
+        """Show info about letter."""
+
         return f'<Letter letter_id={self.letter_id} letter_title={self.letter_title}>'
 
 class Favorite(db.Model):
@@ -45,21 +51,32 @@ class Favorite(db.Model):
 
     __tablename__ = "favorites"
 
-    favorite_id = db.Column(db.Integer)
-    letter_id = db.Column(db.Integer, db.ForeignKey("letter.letter_id"))
+    favorite_id = db.Column(db.Integer, primary_key=True)
+    letter_id = db.Column(db.Integer, db.ForeignKey("letters.letter_id"))
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
 
-
     # create a relationship
-
-    letter = db.relationship("Letter", back_populates="favorites")
-    user = db.relationship("User", back_populates="favorites")
+    # letter = db.relationship("Letter", back_populates="favorites")
+    # user = db.relationship("User", back_populates="favorites")
 
     def __repr__(self):
         return f'<Favorite favorite_id={self.favorite_id}>'
 
 
+def connect_to_db(flask_app, db_uri="postgresql:///letters", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    db.app = flask_app
+    db.init_app(flask_app)
+    print("Connected to the db!")
 
 if __name__ == "__main__":
+    
     from server import app
+    connect_to_db(app)
+
+
+
+
